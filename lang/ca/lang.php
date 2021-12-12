27a321ed9d130ac2fdc3338a55c2d4d9c4eb9da9
Innova Didactic <tecnic@innovadidactic.com>
2021-12-12 22:20:20 +0100

    translation update

---
+++ lang/ca/lang.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Innova Didactic <tecnic@innovadidactic.com>
+ */
+$lang['picker']                = 'Connector Wrap';
+$lang['column']                = 'columnes';
+$lang['box']                   = 'Caixa centrada simple';
+$lang['info']                  = 'Informació';
+$lang['tip']                   = 'Consell';
+$lang['important']             = 'Important';
+$lang['alert']                 = 'Alerta';
+$lang['help']                  = 'Ajuda';
+$lang['download']              = 'Descàrrega';
+$lang['todo']                  = 'Pendent';
+$lang['em']                    = 'especialment ressaltat';
+$lang['hi']                    = 'Destacat';
+$lang['lo']                    = 'menys significatiu';
---
+++ lang/ca/settings.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Innova Didactic <tecnic@innovadidactic.com>
+ */
+$lang['noPrefix']              = 'Quins noms de classe (separats per comes) s\'han d\'excloure del prefix "wrap_"? (* i ? són comodins permesos) ';
+$lang['restrictedClasses']     = 'Restringeix l\'ús del connector a aquestes classes (separades per comes) (es permeten els comodins * i ?) ';
+$lang['restrictionType']       = 'Tipus de restricció, especifica si s\'han d\'incloure o excloure les classes anteriors ';
+$lang['restrictionType_o_0']   = 'Permet totes les classes excepte les anteriors ';
+$lang['restrictionType_o_1']   = 'Restringit a les classes anteriors i cap a altra';
+$lang['syntaxDiv']             = 'Quina sintaxi s\'ha d\'utilitzar al selector de la barra d\'eines per a blocs "wrap"? ';
+$lang['syntaxSpan']            = 'Quina sintaxi s\'ha d\'utilitzar al selector de la barra d\'eines per a "wraps" en línia? ';
+$lang['darkTpl']               = 'Optimitzar els colors per entorns visuals foscos?';
+$lang['emulatedHeadlines']     = 'Utilitzar encapçalaments emulats? (obsolet) ';
